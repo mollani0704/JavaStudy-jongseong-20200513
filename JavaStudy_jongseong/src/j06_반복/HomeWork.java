143bd3bdcd668b60b6d36fb4dd7b8dd253680be9
mollani0704 <mollani0704@gmail.com>
2022-05-20 15:23:25 +0900

    [Update] 웹개발 자바 수업 조건문 숙제

--- JavaStudy_jongseong/src/j06_반복/HomeWork.java
+++ JavaStudy_jongseong/src/j06_반복/HomeWork.java
@@ -3,27 +3,70 @@ package j06_반복;
 public class HomeWork {
 
 	public static void main(String[] args) {
-		/* 숙제(5/19)
-		 * 1.
+		//숙제(5/19)
+		 /* 1.
 		 *     *	
 		 *    **
 		 *   ***
 		 *  ****
 		 * *****
-		 * 
-		 * 2. 
+		 */
+//		for(int i = 0; i < 5; i++) {
+//			for(int j = 0; j < 5-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k = 0; k < i+1; k++) {
+//				
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+		/* 2. 
 		 * *****
 		 *  ****
 		 *   ***
 		 *    **
 		 *     *
-		 * 3. 
+		 */ 
+//		for(int i = 0; i < 5; i++) {
+//			for(int k = 0; k < i; k++) {
+//				System.out.print(" ");
+//			}
+//			
+//			for(int j = 0; j < 5-i; j++) {
+//				System.out.print("*");
+//			}
+//			
+//			System.out.println("");
+//		}
+		
+		/* 3. 
 		 *     *
 		 *    ***
 		 *   *****
 		 *  *******
 		 * *********
 		 */
+		
+		for(int i = 0; i < 5; i++)	{
+			for(int j = 0; j < 5 - i; j++) {
+				System.out.print(" ");	
+			}
+			
+			for(int k = 0; k < i+1; k++) {
+				System.out.print("*");
+			}
+			
+			for(int m = 0; m < i; m++) {
+				System.out.print("*");
+			}
+			
+			
+			System.out.println("");
+		}
 	}
-
+		
 }
+
+
